9a1cb81ce138b624d5ddb68083ce6db2c5221ece
Nucaranlaeg <bennerguy@gmail.com>
2023-10-15 19:55:18 -0500

    Typoes; Druid feature

--- archer.js
+++ archer.js
@@ -13,13 +13,15 @@ classes.archer = {
 		"Reaching level 1, 5, 9, 13, and 17 in this class grants or improves the Ranged Weapons skill.",
 	],
 	"Level 1": [
-		"Archery Style - An archer knows one or more archery styles, and may use any one on a given turn.  Not all ranged weapons are suitable for all styles, and you must select an appropriate weapon for the style you use. Choose one style at level 1.",
+		"Archery Style - An archer knows one or more archery styles, and may use any one on your turn.  Not all ranged weapons are suitable for all styles, and you must select an appropriate weapon for the style you use. Choose one style at level 1.",
 	],
 	"Archery Styles": [
 		"Target - (Any) By taking the time required to line up a shot, you gain Advantage on one attack.  If you could otherwise make more than one attack, instead roll your damage dice one additional time for each attack you do not make.  For instance, a level 10 Archer could normally make 3 attacks for 1d8+3 damage and would do 3d8+3 damage on a hit.",
+		"Sniper - (Requires Target) You may Ready an attack using the Target archery style.",
 		"Volley - (Not crossbows or thrown knives) When making your attacks this turn, choose a 60 foot line starting from you.  Your attacks hit a random creature in that line and cannot score critical hits, but they have Advantage.  To determine which creature is hit, roll 1d6.  The number rolled is the creature hit, counting creatures in the line starting from you.  A roll of 6 is always a miss.  You cannot hit a target further away than you could normally hit with your chosen weapon.",
 		"Indirect Volley - (Requires Volley) When making a volley, you may choose to target a 15 foot square at least half of your weapon's range away and within your weapon's range (instead of a line).  You may ignore obstructions in the way, provided that there is a clear arc with height equal to half the distance to the target.",
-		"Mounted - (Any) You are capable of making accurate shots even when mounted.  You do not suffer disadvantage on ranged attacks when your mount moves.",
+		"Mounted - (Any) You are capable of making accurate shots even when mounted.  You do not suffer disadvantage on ranged
+ attacks when your mount moves.",
 	],
 	"Archer": [
 		"Bull's Eye - Your critical threat range is increased by 1 plus your net Advantage (if you have net Disadvantage, this has no effect).",
--- druid.js
+++ druid.js
@@ -13,12 +13,14 @@ classes.druid = {
 		"Reaching level 1, 5, 9, 13, and 17 in this class grants or improves the Channelling skill.",
 	],
 	"Level 1": [
-		"Wild Shape: As a swift action, you may change your shape to that of a beast, provided you have not been Wild Shaped in the last hour.  While you are in the shape of a beast, you may use a swift action to regain your normal form.  The beast you choose must be <b>Appropriate Challenge Rating</b>, and must be within 1 size class of you.  Wounds carry over between forms, and if you fall unconscious or fall asleep you revert back to your normal form.  You may not channel while Wild Shaped, but you may still cast spells.\nWorked metal does not transform with you and may be destroyed by the transformation depending on your new form (coins are cast, so they do transform). Every other part of your gear transforms with you and is unavailable until you return to your normal form.",
+		"Wild Shape: As a swift action, you may change your shape to that of a beast, provided you have not been Wild Shaped in the last hour.  While you are in the shape of a beast, you may use a swift action to regain your normal form.  The beast you choose must be <b>Appropriate Challenge Rating</b>, and must be within 1 size class of you.  Wounds carry over between forms, and if you fall unconscious (including by sleeping) you revert back to your normal form.  You may not channel while Wild Shaped, but you may still cast spells.\nWhen you are Wild Shaped, your physical attributes become those of the beast, potentially increasing your maximum and current hit points (in addition to other effects).\nWorked metal does not transform with you and may be destroyed by the transformation depending on your new form (coins are cast, so they do transform). Every other part of your gear transforms with you and is unavailable until you return to your normal form.",
 	],
 	"Druid": [
-		"Skinwalker - You may Wild Shape even if you have beed Wild Shaped within the last hour.  If you do, you immediately gain 1 stack of exhaustion.",
-		"Restore the Body - When you return to your normal form, you may heal 1 Wound if you were Wounded while you were Wild Shaped.",
+		"Skinwalker - You may Wild Shape even if you have been Wild Shaped within the last hour.  If you do, you immediately gain 1 stack of exhaustion.",
+		"Restore the Body - When you return to your normal form, you may heal 1 Wound if you were Wounded while you were Wild Shaped. Overchannelling does not count as being Wounded for this feature.",
 		"Natural Magic - You can channel while Wild Shaped, but it counts as two against your daily limit.",
+		"Instincts - When you Wild Shape into a beast with natural abilities, choose one of them and you gain it as long as you are in that shape. You may choose this feature multiple times, increasing the maximum number of natural abilities you gain access to.",
+		"Animal Friendship - Small and smaller beasts do not perceive you as a threat. You can coexist peacefully with larger animals, but ones you have not met before may be aggressive on your first meeting. Purposefully harming an animal that is friendly to you robs you of your natural serenity until the next full moon.",
 	],
 	"General": [
 		"New Spell - Choose a Primal spell and add it to your spell list.  You may choose this feature any number of times.",
