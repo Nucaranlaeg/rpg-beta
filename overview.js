bc8c84f6443c82675cfc008938e38625d2fe7dc8
Nucaranlaeg <bennerguy@gmail.com>
2023-11-15 20:32:54 -0700

    Add advantage note

--- overview.js
+++ overview.js
@@ -3,5 +3,13 @@ ruleSections.overview = [
 		type: RULE_BLOCK.BODY,
 		body: `In this TTRPG, classes are more flexible but also more strongly separate - the class you choose at level 1 grants you abilities that you cannot get any other way, but you have a multitude of choices after that.  If you only choose features from your class, you should be fine, but it is okay to choose features from other classes to make your character play the way you want him to.
 		Your skills are more fluid - you can choose things which fit your character and apply them where appropriate.`,
+	},{
+		type: RULE_BLOCK.SUBTITLE,
+		body: `Advantage`,
+	},{
+		type: RULE_BLOCK.SUBTITLE,
+		body: `Certain situations and features grant Advantage or Disadvantage. Non-situational bonuses and maluses will typically be flat values so you can calculate your total bonus in advance.
+		When you have Advantage, you roll 2d20 and take the higher value. When you have Disadvantage, you take the lower value instead.
+		Subtract your Disadvantage (-D) from your Advantage (+A) before rolling - if you have +A and -D they cancel out and you make a normal roll. If you have net +2A or better, add your net Advantage to your roll (or conversely, subtract your Disadvantage).`,
 	},
 ];
