d1b0196ecc0a9d590ea77d5d63cce0bbc7b7373c
Sam Benner <bennerguy@gmail.com>
2024-01-11 09:51:13 -0700

    Fix skill wrapping

---
+++ items.js
@@ -0,0 +1,14 @@
+const ENCHANT_TARGETS = {
+	ARMOUR: "Armour",
+};
+
+const items = [
+];
+
+const enchantments = [
+	{
+		name: "Fortification",
+		target: ENCHANT_TARGETS.ARMOUR,
+		effect: "The creature wearing this armour ignores the first non-critical wound they would take from each instance of damage.  The attack's damage is still reduced.",
+	},
+];
